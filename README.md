cdce11a45865d04d6b9ae77d36d089faca94e290
yfguan <yfguan@umich.edu>
2018-10-17 10:47:04 -0400

    update README and add debug functionality to gui.py

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 2. export DISPLAY=:0
 3. install X server: https://sourceforge.net/projects/xming/?source=typ_redirect
 https://esc.sh/blog/install-gui-ubuntu-server/
+
+Current Plan:
+C++ Program generates output file. One python program parses output file to our standard format and draw shapes
+
+Reference:
+http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/create_line.html.
+http://effbot.org/tkinterbook/canvas.htm
--- gui.py
+++ gui.py
@@ -1,26 +1,67 @@
 import tkinter
+import sys
 
+
+# def main():
+    # shapes_info = parse(sys.argv[1])
+    # draw(shapes_info)
+
+# def parse(filename):
+#     pass
+
+
+# def draw(shapes_info):
+
+# parse shapes
+coords_to_draw = []
+shape = ''
+with open('output', 'r+') as f:
+    shapes = f.readlines()
+    for line in shapes:
+        type_coords = line.strip(')').split('(')
+        shape = type_coords[0]
+        coords = type_coords[1].split(', ')
+        for coord in coords:
+            coord_pair = coord.split()
+            coords_to_draw.append(coord_pair[0])
+            coords_to_draw.append(coord_pair[1])
+
+
+# draw shapes
 top = tkinter.Tk()
 
-C = tkinter.Canvas(top, bg="blue", height=250, width=300)
+C = tkinter.Canvas(top, bg="white", height=250, width=300)
 
-coord = 10, 50, 240, 210
-arc = C.create_arc(coord, start=0, extent=150, fill="red")
+if shape == 'LINESTRING':
+    C.create_line(coords_to_draw, fill="red")
+elif shape == 'POLYGON':
+    C.create_polygon(coords_to_draw, fill="red")
+elif shape == 'POINT':
+    C.create_point(coords_to_draw, fill="red")
+elif shape == 'ARC':
+    C.create_arc(coords_to_draw, fill="red")
+
 
 C.pack()
 top.mainloop()
 
 
-# from tkinter import *
+# coord = [0, 0, 100, 100]
+# arc = C.create_arc(coord, start=0, extent=180, fill="red")
+# arc = C.create_arc(200, 200, 300, 300, start=0, extent=180, fill="red")
+# arc = C.create_arc(coord, start=0, extent=180, fill="red")
 
-# class Window(Frame):
 
-#     def __init__(self, master=None):
-#         Frame.__init__(self, master)               
-#         self.master = master
+    # from tkinter import *
 
-# root = Tk()
+    # class Window(Frame):
 
-# app = Window(root)
+    #     def __init__(self, master=None):
+    #         Frame.__init__(self, master)               
+    #         self.master = master
 
-# root.mainloop()
+    # root = Tk()
+
+    # app = Window(root)
+
+    # root.mainloop()
